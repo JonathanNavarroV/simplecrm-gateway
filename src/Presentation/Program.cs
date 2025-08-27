ae39f9c4567f6d74f61f6c3b73c1200d6eb9edf6
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2025-08-26 21:39:14 -0400

    Implementación de configuración inicial de CORS

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,11 +1,30 @@
 // Se crea el "builder" de la aplicación: lee args, configura loggin, config, etc.
 var builder = WebApplication.CreateBuilder(args);
 
+// Se registra CORS en el contenedor de servicios
+builder.Services.AddCors(options =>
+{
+    // Se define una política "Default"
+    options.AddPolicy(
+        "Default",
+        policy =>
+        {
+            policy
+                .AllowAnyHeader() // Se permite cualquier header (ej: Authorization)
+                .AllowAnyMethod() // Se permite cualquier método (ej: GET, POST, PUT, DELETE, etc.)
+                .WithOrigins("http://localhost:4200"); // Origen permitido
+        }
+    );
+});
+
 // Contruye la aplicación ASP.NET Core con lo registrado arriba.
 var app = builder.Build();
 
+// Se actuva la política CORS en el pipeline
+app.UseCors("Default");
+
 // Endpoint raíz simple para ver que el host responde.
-app.MapGet("/", () => Results.Text("SimpleCRM Gateway: Ok", "text/plain"));
+app.MapGet("/", () => Results.Text("SimpleCRM Gateway (con CORS): Ok", "text/plain"));
 
 // Endpoint de salud SIN autenticación
 app.MapGet(
